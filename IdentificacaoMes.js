621e35c798d86dd8c49a5aa1581936eaf90e00ed
Janeth Mamani <51747510+JanethMamani@users.noreply.github.com>
2022-05-04 19:43:22 -0300

    Desafio basico de leitura de objeto em TypeScript.

---
+++ IdentificacaoMes.js
@@ -0,0 +1,24 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+let lines = gets().split('\n');
+
+let month = lines.shift();
+
+let months = {
+  "1": "January",
+  "2": "February",
+  "3": "March",
+  "4": "April",
+  "5": "May",
+  "6": "June",
+  "7": "July",
+  "8": "August",
+  "9": "September",
+  "10": "October",
+  "11": "November",
+  "12": "December"
+
+};
+
+print(months[month]);
